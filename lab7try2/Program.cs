b53e9b46fa2ecc3cf679ab5ad402ac9b922620b0
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2022-05-12 17:30:01 +0300

    Thursday version

--- lab7try2/Program.cs
+++ lab7try2/Program.cs
@@ -4,9 +4,25 @@ namespace lab7try2
 {
     class Program
     {
+        
+        public static string fileName = @"C:\Users\HP\RiderProjects\lab6try1\data_edited.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            ReadFile text_to_analyze = new ReadFile(fileName);
+
+            string input_text;
+            input_text = Console.ReadLine();
+            
+            
+            string[] words = input_text.Split(" ");
+
+          //  Console.WriteLine(text_to_analyze.SearchInDictionary(words));
+          
+            //text_to_analyze.SearchInDictionary(words);
+            text_to_analyze.PrintOddWords(text_to_analyze.SearchInDictionary(words));
+
         }
     }
 }
---
+++ lab7try2/ReadFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab7try2
+{
+    public class ReadFile
+    {
+        private string[] lines;
+        
+        public ReadFile(string fileName)
+        {
+            lines = System.IO.File.ReadAllLines(fileName);
+        }
+
+       
+        
+        public List<string> SearchInDictionary(string[] words)
+        {
+            List<string> list_of_strings = new List<string>(); 
+           // foreach (var line in lines)
+           // {
+                foreach (var word in words)
+                {
+                    if (lines.Contains(word))
+                    {
+                        list_of_strings.Add(word);
+                    }
+                }
+           // }
+            return list_of_strings;
+        }
+
+        public void PrintOddWords(List<string> list_of_strings)
+        {
+            foreach (var single_string in list_of_strings)
+            {
+                Console.WriteLine(single_string);
+            }
+        }
+        
+      
+    }
+}
