33ee345180f15f5558341a06a3dc5cd925b11bcd
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2022-05-18 23:38:35 +0300

    Work in progress

---
+++ lab7try2/LongestSubsequence.cs
@@ -0,0 +1,60 @@
+﻿namespace lab7try2
+{
+    public class LongestSubsequence
+    {
+        
+        public static string FindCommon(string a, string b)
+        {
+            int maxlength;
+
+            if (a.Length > b.Length)
+            {
+                maxlength = a.Length;
+            }
+            else
+            {
+                maxlength = b.Length;
+            }
+
+            char[] copyA;
+            char[] copyB;
+
+            copyA = new char[maxlength];
+            copyB = new char[maxlength];
+
+
+            for (int i = 0; i < maxlength; i++)
+            {
+                copyA[i] = a[i];
+            }
+            for (int i = 0; i < maxlength; i++)
+            {
+                copyB[i] = b[i];
+            }
+
+            string res = "";
+
+
+            for (int i = 0; i < maxlength; i++)
+            {
+                for (int j = 0; j < maxlength; j++)
+                {
+                    if (copyA[i] == copyB[j])
+                    {
+                        res += copyA[i];
+                    }
+                }
+                
+            }
+
+            return res;
+            
+            
+            
+
+
+            // string aCopy = new string[];
+
+        } 
+    }
+}
---
+++ lab7try2/TableOfCommons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab7try2
+{
+    public class TableOfCommons
+    {
+        private int[,] Table;
+        
+
+        public TableOfCommons(int length)
+        {
+            
+            Table = new int[length,length];
+
+            for(int row = 0; row < length; row++)
+            {
+                for (int column = 0; column < length; column++)
+                {
+                    Console.Write(Table[row,column] + " ");
+                }
+                Console.WriteLine("");
+                
+            }
+        }
+        
+    }
+}
